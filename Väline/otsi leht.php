58c4706fc646e904b16e1e6c1d9625ab27476ce0
Illimar Kahar <illimar.kahar@khk.ee>
2014-02-09 12:05:32 +0200

    search page / php

--- Väline/otsi leht.php
+++ Väline/otsi leht.php
@@ -1,7 +1,43 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: pc
- * Date: 9.02.14
- * Time: 11:58
- */ 
+$search_output = "";
+if(isset($_POST['searchquery']) && $_POST['searchquery'] != ""){
+    // run code if condition meets here
+}
+?>
+<html>
+<head>
+    <link rel='stylesheet' href='style.css'/>
+    <script src='script.js'></script>
+</head>
+<body>
+<left>
+    <h1 style="color: #000000; font-size: 15px">Keretüüp</h1>
+    <form action="./results.php" method="get">
+        <input type="text" name="input" size="5">
+
+    </form>
+
+    <h1 style="color: #000000; font-size: 15px">Mark</h1>>
+    <form action="./results.php" method="get">
+        <input type="text" name="input" size="5">
+
+    </form>
+
+    </form>
+
+    <h1 style="color: #000000; font-size: 15px">Mudel</h1>>
+    <form action="./results.php" method="get">
+        <input type="text" name="input" size="5">
+
+    </form>
+
+    <h1 style="color: #000000; font-size: 15px">Aasta</h1>>
+    <form action="./results.php" method="get">
+        <input type="text" name="input" size="5">
+
+    </form>
+    <input type="submit" value="Otsi"/>
+</left>
+
+</body>
+</html>
